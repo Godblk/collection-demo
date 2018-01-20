9da55b09afd80c8e216db2aa734d58ecd22f3514
godblk <1284541185@qq.com>
2018-01-20 13:43:16 +0800

    jsdfhisudfs

--- CollectionListDemo/ViewController.m
+++ CollectionListDemo/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "JHCollectionViewFlowLayout.h"
 
 @interface ViewController ()
-
+<UICollectionViewDataSource, UICollectionViewDelegate, JHCollectionViewDelegateFlowLayout>
+@property (nonatomic, strong) UICollectionView *collectionView;
 @end
 
 @implementation ViewController
@@ -17,13 +19,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    JHCollectionViewFlowLayout *jhFlowLayout = [[JHCollectionViewFlowLayout alloc] init];
+    jhFlowLayout.scrollDirection = UICollectionViewScrollDirectionVertical;
+    jhFlowLayout.itemSize = CGSizeMake(50, 50);
+    jhFlowLayout.minimumInteritemSpacing = 30;
+    jhFlowLayout.minimumLineSpacing = 30;
+    jhFlowLayout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10);
+    
+    _collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:jhFlowLayout];
+    _collectionView.delegate = self;
+    _collectionView.dataSource = self;
+    [self.view addSubview:_collectionView];
+    
+    [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"UICollectionViewCell"];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UICollectionViewDataSource
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 3;
 }
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 13;
+}
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"UICollectionViewCell" forIndexPath:indexPath];
+    cell.contentView.backgroundColor = [UIColor whiteColor];
+    
+    return cell;
+}
+
+#pragma mark - JHCollectionViewDelegateFlowLayout
+- (UIColor *)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout backgroundColorForSection:(NSInteger)section
+{
+    return [@[
+              [UIColor redColor],
+              [UIColor greenColor],
+              [UIColor yellowColor]
+              ] objectAtIndex:section];
+}
+
 
 
 @end
